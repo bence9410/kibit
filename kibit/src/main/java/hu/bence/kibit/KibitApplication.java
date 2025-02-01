f6e2294163c2899c7848fc7f97b668af72277150
Bence <nembence1994@gmail.com>
2025-02-01 10:32:09 +0100

    Add project.

---
+++ kibit/src/main/java/hu/bence/kibit/KibitApplication.java
@@ -0,0 +1,13 @@
+package hu.bence.kibit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KibitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KibitApplication.class, args);
+	}
+
+}
---
+++ kibit/src/test/java/hu/bence/kibit/KibitApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bence.kibit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KibitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
